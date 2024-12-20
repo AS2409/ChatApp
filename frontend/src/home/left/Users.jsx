bdcd205054f8d55cf78d2b9cabbc60b1985fc763
Vedang Mehta <vedangmehta07.com>
2024-12-21 00:35:46 +0530

    jwt file added

--- frontend/src/home/left/Users.jsx
+++ frontend/src/home/left/Users.jsx
@@ -3,7 +3,7 @@ import User from "./User";
 function Users() {
   return (
     <div
-      className="flex-aditi py-2 overflow-y-auto"
+      className=" py-2 overflow-y-auto "
       style={{ maxHeight: "calc(84vh-1vh)"}}
     >
       <User></User>
