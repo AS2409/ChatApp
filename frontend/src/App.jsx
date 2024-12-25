bb930da1054b4d5ed14c4268df78e832d5099642
Vedang Mehta <vedangmehta07.com>
2024-12-25 14:34:53 +0530

    Added Loading Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import { useAuth } from "./context/AuthProvider";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Loading from "./components/Loading";
 
 function App() {
   const { authUser, setAuthUser } = useAuth();
@@ -36,6 +37,7 @@ function App() {
           element={authUser ? <Navigate to={"/"} /> : <Signup />}
         />
       </Routes>
+      {/* <Loading></Loading> */}
     </>
   );
 }
---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <>
+      <div className="flex h-screen items-center  justify-center bg-cyberNavy ">
+        <div className="flex w-52 flex-col gap-4">
+          <div className="skeleton h-32 bg-lavenderBlue w-full"></div>
+          <div className="skeleton h-5 bg-deepPurple w-28"></div>
+          <div className="skeleton h-5 bg-lavenderBlue w-full"></div>
+          <div className="skeleton h-5 bg-deepPurple   w-full"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Loading;
